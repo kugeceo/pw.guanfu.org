370dc5016b9e09fd82192901e83d5d1e0e81c349
鲁虺 <37980424+kugeceo@users.noreply.github.com>
2025-01-20 13:40:18 +0800

    Add files via upload

---
+++ js/tj.js
@@ -0,0 +1,7 @@
+document.writeln("<script type=\"text/javascript\">");
+document.writeln("    (function(c,l,a,r,i,t,y){");
+document.writeln("        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};");
+document.writeln("        t=l.createElement(r);t.async=1;t.src=\"https://www.clarity.ms/tag/\"+i;");
+document.writeln("        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);");
+document.writeln("    })(window, document, \"clarity\", \"script\", \"lt6ugb4mny\");");
+document.writeln("</script>");
